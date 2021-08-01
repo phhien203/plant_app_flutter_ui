1255ec2aa342fb8a3d94d022ca14aee4e68a3547
Pham Huu Hien <phhien203@gmail.com>
2021-08-01 13:44:45 +0700

    build header with search box Widget

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'header_with_searchbox.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[HeaderWithSearchBox(size: size)],
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app_flutter_ui/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        onPressed: () {},
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+      ),
+    );
+  }
+}
